00c10d948681a548b56cd45a404f4772fbbcddb9
Amirhossein Kiani <Amirhossein.Kiani@hennepin.us>
2025-06-27 23:01:55 -0500

    Add Editor component to the project

---
+++ cameravision/src/renderer/src/Editor.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Editor() {
+    return (
+        <div>
+            <h1>CameraVision Editor</h1>
+            <p>Welcome to the CameraVision Editor. Start editing your project here.</p>
+        </div>
+    );
+}
+
+export default Editor;
