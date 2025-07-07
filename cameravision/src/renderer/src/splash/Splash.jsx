ff10004c50f9931683e8ac29916cea18af95ee81
Amirhossein Kiani <kiani014@umn.edu>
2025-07-07 13:43:14 -0500

    chore: Enhance splash screen with background image and attribution, remove unused loading styles

--- cameravision/src/renderer/src/splash/Splash.jsx
+++ cameravision/src/renderer/src/splash/Splash.jsx
@@ -1,10 +1,30 @@
 import '../assets/main.css'
-
+import splashBg from '../assets/traffic_splash_bg.jpg'
 function Splash() {
   return (
-    <div>
-      <h1 className="!text-black">Welcome to the Splash Screen</h1>
-      <p>This is a simple splash screen for our application.</p>
+    <div
+      className="relative min-h-screen min-w-screen items-center justify-center bg-main-600 text-white"
+      style={{ backgroundImage: `url(${splashBg})`, backgroundSize: 'cover' }}
+    >
+      <div className="absolute z-10 h-screen w-screen backdrop-blur-sm bg-white/[0.1]"></div>
+      <div className="absolute z-20 bottom-2 left-2">
+        Photo by{' '}
+        <a
+          href="https://unsplash.com/@berkinuregen?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Berkin Üregen
+        </a>{' '}
+        on{' '}
+        <a
+          href="https://unsplash.com/photos/traffic-light-under-blue-sky-during-daytime-gf9J4fyJKD0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Unsplash
+        </a>
+      </div>
     </div>
   )
 }
