b465f3dadb5321834191413774040ee830647d1b
Amirhossein Kiani <amirhkiani98@yahoo.com>
2025-07-15 20:12:56 -0500

    feat: Update RTSP recording function to include record ID; modify ASGI setup for Django channels

--- backend/processor/asgi.py
+++ backend/processor/asgi.py
@@ -1,8 +1,8 @@
 import os
-from django.core.asgi import get_asgi_application
+from django
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'processor.settings')
-
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
 
