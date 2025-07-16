ff731ede5ad0f5402712b2b9928e9096fda4d91d
Amirhossein Kiani <amirhkiani98@yahoo.com>
2025-07-15 22:31:08 -0500

    feat: Refactor ASGI configuration; consolidate routing and remove obsolete routing file

--- backend/processor/asgi.py
+++ backend/processor/asgi.py
@@ -1,9 +1,15 @@
 import os
 import django
 from channels.routing import get_default_application
-
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from processor.routing import websocket_urlpatterns   # Import your routing module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'processor.settings')
 django.setup()
 application = get_default_application()
 
-from backend.routing import application
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+})
---
+++ backend/processor/routing.py
@@ -0,0 +1,7 @@
+from django.urls import re_path
+from backend.cronjob.consumers import ProgressConsumer  # Adjust path to your app
+
+
+websocket_urlpatterns = [
+    re_path(r"ws/recording_progress/(?P<record_id>\w+)/$", ProgressConsumer.as_asgi()),
+]
