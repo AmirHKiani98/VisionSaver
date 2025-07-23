995f8e39ebd794d6f7e64c0a76306484ff971b16
Amirhossein Kiani <Amirhossein.Kiani@hennepin.us>
2025-07-23 14:25:10 -0500

    Refactor code structure for improved readability and maintainability

--- backend/ai/tests.py
+++ backend/ai/tests.py
@@ -1,11 +1,26 @@
 from django.test import TestCase
-
+from .views import load_model
+from django.conf import settings
+import cv2
 class AiAppTestCase(TestCase):
     """
     Template test case for the 'ai' Django app.
     """
-    def test_sample(self):
+    def test_video_image(self):
         # Example test: always passes
-        self.assertTrue(True)
-
+        model = load_model()
+        video_path = settings.MEDIA_ROOT + "/503.mkv"
+        video_capture = cv2.VideoCapture(video_path)
+        fps = video_capture.get(cv2.CAP_PROP_FPS)
+        target_time = 130 # seconds into the video (2:08)
+        frame_number = int(target_time * fps)
+        video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        success, frame = video_capture.read()
+        if not success:
+            self.fail("Failed to read frame from video.")
+        # Plot frame
+        results = model(frame)
+        print(results)
+        
+        
 
--- backend/ai/views.py
+++ backend/ai/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from ultralytics import YOLO
+
+def load_model():
+    """
+    Load the YOLO model and return a response.
+    """
+    model = YOLO('yolov8n.pt')  # Load a pre-trained YOLO model
+    return model
