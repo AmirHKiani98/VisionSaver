908999812fb6d843b2130c29231e318ad2d78662
Amirhossein Kiani <Amirhossein.Kiani@hennepin.us>
2025-07-30 14:18:34 -0500

    Implement code changes to enhance functionality and improve performance

--- backend/ai/car_detection.py
+++ backend/ai/car_detection.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+import pandas as pd
 
 from django.conf import settings
 logger = settings.APP_LOGGER
@@ -11,15 +12,17 @@ class CarDetection():
         """
         Initialize the CarDetection instance.
         """
+        self.results_df = None
         self.model = model
-        self.results = {}
         self.load_video(video_path)
         if tracker_config is not None:
             self.tracker = DeepSort(**tracker_config)
         else:
             self.tracker = DeepSort(max_age=5)
         logger.info(f"CarDetection initialized with model {model} and video {video_path}")
+        self.results_df = None
         self.get_results_from_video(divide_time)
+        
         
     def load_video(self, video_path):
         """
@@ -43,13 +46,24 @@ class CarDetection():
         detections = []
         results = self.model(image)[0]
         objects_of_interest = [2, 3, 5, 7]
-
+        objects = []
         if hasattr(results, 'boxes') and results.boxes is not None:
             for box, cls, conf in zip(results.boxes.xyxy, results.boxes.cls, results.boxes.conf):
                 if int(cls) in objects_of_interest:
                     x1, y1, x2, y2 = map(float, box)
+                    # AVG RGB
+                    avg_rgb = self.average_rgb(image, [x1, y1, x2, y2])
+                    
+                    objects.append({
+                        'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2,
+                        'avg_r': avg_rgb[0] if avg_rgb else None,
+                        'avg_g': avg_rgb[1] if avg_rgb else None,
+                        'avg_b': avg_rgb[2] if avg_rgb else None,
+                        'confidence': float(conf),
+                        'class_id': int(cls),
+                    })
                     detections.append(([x1, y1, x2, y2], float(conf), 'vehicle'))
-
+        return objects
         # Pass detections to tracker
         tracks = self.tracker.update_tracks(detections, frame=image)
 
@@ -60,15 +74,24 @@ class CarDetection():
                 continue
             track_id = track.track_id
             tlwh = [float(x) for x in track.to_tlwh()]
+            # Find the YOLO box that matches this track (you may need to keep a mapping)
+            # For simplicity, let's assume you can get the last YOLO box for this track
+            # You need to implement this mapping logic in your pipeline
+            yolo_box = track.last_detection_box if hasattr(track, 'last_detection_box') else None
+            if yolo_box is not None:
+                x1, y1, x2, y2 = map(int, yolo_box)
+                checked = "yolo"
+            else:
+                # fallback to tracker box if YOLO box is not available
+                x1, y1, w, h = map(int, tlwh)
+                x2, y2 = x1 + w, y1 + h
+                checked = "tracker"
             tracked_objects.append({
                 'track_id': track_id,
-                'tlx': tlwh[0],
-                'tly': tlwh[1],
-                'width': tlwh[2],
-                'height': tlwh[3],
-                'label': 'vehicle'
+                'label': 'vehicle',
+                'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2,
+                'checked': checked
             })
-
         return tracked_objects
 
         
@@ -85,7 +108,7 @@ class CarDetection():
         if fps <= 0:
             logger.error("Invalid FPS value. Cannot extract frames.")
             raise ValueError("Invalid FPS value. Cannot extract frames.")
-        
+        df = pd.DataFrame(columns=['time', 'frame_number', "x1", "y1", "x2", "y2", "confidence", "class_id"])
         for i in np.arange(0, self.duration, divide_time):
             print(f"Progress : {round((i/self.duration) * 10000)/100}%")
             frame_number = int(i * fps)
@@ -95,9 +118,38 @@ class CarDetection():
                 logger.warning(f"Failed to read frame at {frame_number}. Skipping.")
                 continue
             data = self.detect_and_track(frame)
+            for obj in data:
+                new_row = pd.DataFrame([{
+                    'time': i,
+                    'frame_number': frame_number,
+                    'x1': obj['x1'],
+                    'y1': obj['y1'],
+                    'x2': obj['x2'],
+                    'y2': obj['y2'],
+                    'confidence': obj.get('confidence', 0.0),
+                    'class_id': obj.get('class_id', 0)
+                }])
+                df = pd.concat([df, new_row], ignore_index=True)
 
-            self.results[i] = {
-            'timestamp': float(i),
-            'frame_number': frame_number,
-            'objects': data
-            }
+        self.results_df = df
+        return df
+
+    def average_rgb(self, frame, bbox) -> list[float] | None:
+        """
+        Calculate the average RGB values of the bbox inside a frame.
+        :param frame: The video frame as a numpy array.
+        :param bbox: Bounding box in the format [x1, y1, x2, y2].
+        """
+        x1, y1, x2, y2 = map(int, bbox)
+        if x1 < 0 or y1 < 0 or x2 > frame.shape[1] or y2 > frame.shape[0]:
+            logger.error("Bounding box is out of frame bounds.")
+            return None
+        roi = frame[y1:y2, x1:x2]
+        if roi.size == 0:
+            logger.warning("Region of interest is empty.")
+            return None
+        avg_color = cv2.mean(roi)[:3]
+        return [avg_color[2], avg_color[1], avg_color[0]] # [R, G, B] order
+        
+        
+        
--- backend/ai/tests.py
+++ backend/ai/tests.py
@@ -23,10 +23,7 @@ class AiAppTestCase(TestCase):
         self.output_dir = os.path.join(os.path.dirname(__file__), 'test')
         self.assertTrue(os.path.isfile(self.video_path), f"Video file not found at {self.video_path}")
 
-    def test_video_image(self):
-        self.assertTrue(os.path.isfile(self.video_path), f"Video file not found at {self.video_path}")
-        if os.path.isfile(os.path.join(self.output_dir, 'car_detection_results.json')):
-            return
+    def test_video_image(self):        
         
         # Load YOLOv8 model
         model = YOLO('yolov8n.pt', verbose=False)
@@ -36,54 +33,56 @@ class AiAppTestCase(TestCase):
             divide_time=0.5
         )
 
-        # Check that results exist
-        self.assertIsInstance(detector.results, dict)
-        self.assertGreater(len(detector.results), 0, "No frames were processed")
-        # Store the data inside the test folder
-        os.makedirs(self.output_dir, exist_ok=True)
-        with open(os.path.join(self.output_dir, 'car_detection_results.json'), 'w') as f:
-            # Convert all keys to str to ensure JSON serializability
-            def convert_keys_to_str(obj):
-                if isinstance(obj, dict):
-                    return {str(k): convert_keys_to_str(v) for k, v in obj.items()}
-                elif isinstance(obj, list):
-                    return [convert_keys_to_str(i) for i in obj]
-                else:
-                    return obj
-            json.dump(convert_keys_to_str(detector.results), f, indent=4)
+        
+        
+        
 
         # Check if the file exists
-        self.assertTrue(os.path.isfile(os.path.join(self.output_dir, 'car_detection_results.json')), "Results file was not created")
+        objects_df = detector.results_df
+        self.assertIsNotNone(objects_df, "Results DataFrame is None")
+        self.assertGreater(len(objects_df), 0, "No objects detected in the video") # type: ignore
+        os.makedirs(self.output_dir, exist_ok=True)
+        objects_df.to_csv(os.path.join(self.output_dir, 'car_detection_results.csv'), index=False) # type: ignore
+        self.play_annotated_video(objects_df)
+        
+        
+    # def test_play_annotated_video(self):
+    #     # Test playing the annotated video
+    #     with open(os.path.join(self.output_dir, 'car_detection_results.json'), 'r') as f:
+    #         detector = json.load(f)
+    #     self.play_annotated_video(detector)
 
-    def test_play_annotated_video(self):
-        # Test playing the annotated video
-        with open(os.path.join(self.output_dir, 'car_detection_results.json'), 'r') as f:
-            detector = json.load(f)
-        self.play_annotated_video(detector)
-
-    def play_annotated_video(self, detector: dict, fps=10):
+    def play_annotated_video(self, objects_df, fps=10):
         delay = int(1000 / fps)
-        sorted_keys = sorted(detector.keys())
+        objects_df = objects_df.copy()
+        objects_df = objects_df.sort_values(by='time')
         cap = cv2.VideoCapture(self.video_path)
-        for timestamp in sorted_keys:
-            result = detector[timestamp]
-            frame_number = result['frame_number']
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        last_frame_number = None
+        group_by_time = objects_df.groupby(['time', 'frame_number'])
+        for names, group in group_by_time:
+            timestamp, frame_number = names
+            
+            print(f"Processing timestamp: {timestamp}")
+            # Only seek if not the next frame
+            if last_frame_number is None or frame_number != last_frame_number + 1:
+                cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
             success, frame = cap.read()
             if not success:
                 continue
-            frame = self.draw_detections(frame, result['objects'])
+            frame = self.draw_detections(frame, group)
             cv2.imshow('Annotated Video', frame)
             if cv2.waitKey(delay) & 0xFF == ord('q'):
                 break
+            last_frame_number = frame_number
         cap.release()
         cv2.destroyAllWindows()
 
     def draw_detections(self, frame, objects):
-        for obj in objects:
-            x1, y1, x2, y2 = map(int, obj['bbox'])
-            track_id = obj['track_id']
-            label = obj['label']
+        for index, row in objects.iterrows():
+            x1, y1 = int(row['x1']), int(row['y1'])
+            x2, y2 = int(row['x2']), int(row['y2'])
+            track_id = row['track_id'] if 'track_id' in row else 0
+            label = row['label'] if 'label' in row else 'unknown'
 
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(frame, f"{label} ID:{track_id}", (x1, y1 - 10),
